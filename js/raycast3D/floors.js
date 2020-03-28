db3c2b531403cbabc1cb4ae0e32d7fc537450593
Christer Kaitila <breakdance@mcfunkypants.com>
2020-03-28 07:24:57 -0700

    optimized floor and ceiling rendering

--- js/raycast3D/floors.js
+++ js/raycast3D/floors.js
@@ -16,8 +16,16 @@ class floorClass
         this.floor = document.createElement("DIV");
         this.floor.className = "floordiv";
 
+        this.floorGraphic = document.createElement("DIV");
+        this.floorGraphic.className = "floorgraphic";
+        this.floor.appendChild(this.floorGraphic);
+
         this.ceiling = document.createElement("DIV");
         this.ceiling.className = "ceilingdiv";
+
+        this.ceilingGraphic = document.createElement("DIV");
+        this.ceilingGraphic.className = "ceilinggraphic";
+        this.ceiling.appendChild(this.ceilingGraphic);
 
         this.blackScreen = document.createElement("DIV");
         this.blackScreen.className = "blackScreen";
@@ -45,14 +53,25 @@ class floorClass
         this.floor.style.transform += "rotate(" + angle.toString() + "deg)";
         
         //performance eater
-        this.floor.style.backgroundPosition = (position.x%100).toString() + "%" + (position.y%100).toString() + "%";
+        // this.floor.style.backgroundPosition = (position.x%100).toString() + "%" + (position.y%100).toString() + "%";
+        // simulate a backgroundPosition change by sliding around a child div
+        const floorScrollScale = 10; // pixel dist for game units travelled
+        const bgtilesize = 320;
+        var xform = "translate3d("
+                        +((position.x*floorScrollScale)%bgtilesize).toString()+"px, "
+                        +((position.y*floorScrollScale)%bgtilesize).toString()+"px, "
+                        +"0px"
+                        +")";
+        this.floorGraphic.style.transform = xform;
 
         this.ceiling.style.transformOrigin = "center center";
         this.ceiling.style.transform = "rotate3d(1, 0, 0, 90deg) translate3d(-3360px, 0px, 4096px)"; 
         this.ceiling.style.transform += "rotate(" + angle.toString() + "deg)";
         
         //performance eater
-        this.ceiling.style.backgroundPosition = (position.x%100).toString() + "% " + (position.y%100).toString() + "%";
+        //this.ceiling.style.backgroundPosition = (position.x%100).toString() + "% " + (position.y%100).toString() + "%";
+        this.ceilingGraphic.style.transform = xform;
+
     }
 }
 
